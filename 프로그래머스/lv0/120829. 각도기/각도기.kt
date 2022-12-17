b7ec809b3b3daa674f3ffdc144ce757cefed68ae
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2022-12-18 00:22:34 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(angle: Int): Int {
+    if(angle <90){ return 1}
+       
+        else if(angle == 90){return 2}
+        
+        else if(90 < angle && angle < 180){ return 3}
+       
+        else {  return 4}
+      
+    }
+}
