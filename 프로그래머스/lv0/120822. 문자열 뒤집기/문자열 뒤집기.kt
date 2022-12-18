3f1138c50a06e30117ce6fb200f514974173f07a
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2022-12-18 16:35:02 +0900

    [level 0] Title: 문자열 뒤집기, Time: 7.47 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(my_string: String): String {
+        //코틀린에서는 charat대신 그냥 인덱스로 분할 가능
+        
+        var answer: String = my_string.reversed()
+        
+        return answer
+    }
+}
