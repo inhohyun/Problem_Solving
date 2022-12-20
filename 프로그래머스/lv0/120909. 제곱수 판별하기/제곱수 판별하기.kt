825f184e9d628d569fff01d9628df9ceabe1b531
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2022-12-20 14:21:08 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    //제곱수 판별
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        for(i in 1..n){
+            if(i*i == n)
+            return 1 
+            
+        }
+        return 2
+    }
+}
