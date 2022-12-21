d71f01a33579a2d0c939392d153bb179ce5c3949
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2022-12-21 13:16:06 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 189.08 ms, Memory: 47.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val (n) = readLine()!!.split(' ').map(String::toInt)
+    for(i in 1..n)
+    {
+        for(j in 0..i-1){
+               print("*")
+            
+        }
+    println()
+    }
+
+    
+}
