48f1550acc798e297a353d8797e72a7d609a5bd8
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2022-12-21 17:36:55 +0900

    [level 0] Title: 합성수 찾기, Time: 0.01 ms, Memory: 62 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        
+        for(i in 1..n){
+            var count = 0
+            for(j in 1..i){
+                if(i % j == 0)
+                count += 1
+            }
+            if(count >= 3)
+            answer++
+        }
+        return answer
+    }
+}
