feafc3ef00800093343d16a3cc21d8364122733a
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2022-12-22 18:54:15 +0900

    [level 0] Title: 진료 순서 정하기, Time: 20.95 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.kt
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.kt
@@ -1,17 +1,6 @@
 class Solution {
-    //크기에 따른 순서 정하기, 큰 수면 
-    //save와 같은 수 = n-i 저장
-    fun solution(emergency: IntArray): IntArray {
-        var answer: IntArray = IntArray(emergency.size)
-        var n = emergency.size // 저장할 수
-        var save = emergency.sorted()
-        for(i in 0..emergency.size-1){
-            for(j in 0..save.size-1){
-                if(emergency[i] == save[j]){
-                    answer[i] = n-j
-                }
-            }
-        }
-        return answer
-    }
+    fun solution(emergency: IntArray): IntArray =  emergency.map{
+            emergency.sortedDescending().indexOf(it)+1
+      //  sortedDescending()
+        }.toIntArray()
 }
