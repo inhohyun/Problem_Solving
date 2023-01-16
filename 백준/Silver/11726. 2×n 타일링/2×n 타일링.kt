6d441f283bd3ea74b24aa5e94c821f2f7b29424d
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-01-16 18:30:51 +0900

    [Silver III] Title: 2×n 타일링, Time: 124 ms, Memory: 13564 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+
+fun main(args: Array<String>): Unit = with(Scanner(System.`in`)) {
+    var n = nextInt()
+    var d = IntArray(1001)
+    d[0] = 1
+    d[1] = 1
+    for (i in 2..n){
+        d[i] = d[i-1]+d[i-2]
+        d[i] %= 10007
+    }
+
+    print(d[n])
+}
+
+
+
+
+
+
