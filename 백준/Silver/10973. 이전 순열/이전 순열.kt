dbebba9486972bb55fef54c88108c1f526ae4f34
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-01-16 09:28:46 +0900

    [Silver III] Title: 이전 순열, Time: 496 ms, Memory: 27200 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.kt
@@ -0,0 +1,60 @@
+import java.util.*
+
+var n = 0
+var arr = IntArray(n)
+fun main(args: Array<String>): Unit = with(Scanner(System.`in`)) {
+    n = nextInt()
+    arr = IntArray(n)
+    for (i in 0 until n) {
+        arr[i] = nextInt()
+    }
+
+    if (per(arr)) {
+        for (i in 0 until n){
+            print("${arr[i]} ")
+        }
+    } else {
+        print("-1")
+    }
+}
+
+//변수 두 개(swap할 위치)를 활용해 다음 순열을 구하기
+fun per(arr:IntArray): Boolean {
+    var i = arr.size-1
+    while (i >0 && arr[i-1] <= arr[i]){
+        i -= 1
+    }
+
+    if(i <= 0) return false
+
+    var j = arr.size-1
+    while (arr[j] >= arr[i-1]){
+        j-=1
+    }
+    //위치 swap
+    var temp = arr[i-1]
+    arr[i-1] = arr[j]
+    arr[j] = temp
+
+    j = arr.size-1
+    //i번째부터 마지막까지 수 뒤집기
+    while (i<j){
+        temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+        i++
+        j--
+    }
+        return true
+}
+
+
+
+
+
+
+
+
+
+
+
