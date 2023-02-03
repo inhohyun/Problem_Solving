609ee4b10baf9057b5b568a5a153fba440aa46f0
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-02-03 15:51:10 +0900

    [level 2] Title: 타겟 넘버, Time: 0.42 ms, Memory: 61.9 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.kt
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.kt
@@ -1,21 +1,21 @@
 class Solution {
-    //numbers안에 수로 target을 만드는 방법
     fun solution(numbers: IntArray, target: Int): Int {
         var answer = 0
-     
- 
-       fun dfs(index: Int, result: Int){
-        if(index == numbers.size){
-            if(result == target){
-              answer++
+        
+        fun dfs(index:Int, temp:Int){
+            if(index < numbers.size-1)
+            {
+                dfs(index+1, temp+numbers[index])
+                dfs(index+1, temp-numbers[index])
+            }
+            else 
+            {
+                if(temp+numbers[index] == target || temp-numbers[index] == target){
+                    answer++
+                }
             }
         }
-        else{
-            dfs(index+1, result+numbers[index])
-            dfs(index+1, result-numbers[index])
-        }
-       }
-       dfs(0,0)
-          return answer
+        dfs(0,0)
+        return answer
     }
 }
