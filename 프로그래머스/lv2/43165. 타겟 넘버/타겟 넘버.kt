a89f2be2e42c19375badbf5209d8db58827e8e8a
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-02-06 10:50:26 +0900

    [level 2] Title: 타겟 넘버, Time: 0.48 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.kt
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.kt
@@ -1,21 +1,19 @@
 class Solution {
     fun solution(numbers: IntArray, target: Int): Int {
         var answer = 0
+      
         
-        fun dfs(index:Int, temp:Int){
-            if(index < numbers.size-1)
-            {
+        fun dfs(index: Int, temp:Int){
+            if(index < numbers.size-1){
                 dfs(index+1, temp+numbers[index])
                 dfs(index+1, temp-numbers[index])
             }
-            else 
-            {
-                if(temp+numbers[index] == target || temp-numbers[index] == target){
-                    answer++
-                }
+            else{
+                if(temp+numbers[index] == target) answer++
+                if(temp-numbers[index] == target) answer++
             }
         }
-        dfs(0,0)
+          dfs(0,0)
         return answer
     }
 }
