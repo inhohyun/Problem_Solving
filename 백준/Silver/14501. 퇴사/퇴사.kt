b29f80260a39a19329e95a965cc57b808fad84ad
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-02-08 17:17:25 +0900

    [Silver III] Title: 퇴사, Time: 124 ms, Memory: 13616 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.kt
+++ 백준/Silver/14501. 퇴사/퇴사.kt
@@ -1,45 +1,34 @@
-import java.util.*
+    import java.util.Scanner
 
-var n = 0
-var t = IntArray(0)
-var p = IntArray(0)
-var ans = 0
+var T = IntArray(0)
+    var P = IntArray(0)
+    var n = 0
+    var max = 0
+    fun main(args: Array<String>): Unit = with(Scanner(System.`in`)) {
+         n = nextInt()
+        T = IntArray(n+1)
+        P = IntArray(n+1)
 
-//모든 일 수를 탐색
-fun main(args: Array<String>): Unit = with(Scanner(System.`in`)) {
-    n = nextInt()
-    t = IntArray(n + 1)
-    p = IntArray(n + 1)
-    for (i in 1.. n) {
-        t[i] = nextInt()
-        p[i] = nextInt()
+        for (i in 1..n){
+            T[i] = nextInt()
+            P[i] = nextInt()
+        }
+        go(1, 0)
+        print(max)
     }
 
-    go(1, 0)
-    println(ans)
+    fun go(day:Int, sum:Int){
 
-}
 
-//최대 수익을 리턴하는 함수, 파라미터: 현재 날짜, 현재 더한 값
-//더할껀지 말껀지 선택하는 문제
-//sum : day-1일까지 수익
-fun go(day: Int, sum: Int) {
-    if (day == n + 1) {
-        if (ans < sum) ans = sum
-        return
+        if (day == n+1){
+            if (max < sum)max = sum
+            return
+        }
+        if (day > n+1) return
+
+
+
+            go(day+T[day], sum+P[day])
+            go(day+1, sum)
+
     }
-    if (day > n + 1) return
-
-    go(day + 1, sum)
-    go(day + t[day], sum + p[day])
-
-}
-
-
-
-
-
-
-
-
-
