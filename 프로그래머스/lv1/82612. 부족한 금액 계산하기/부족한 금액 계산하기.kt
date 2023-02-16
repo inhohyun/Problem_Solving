c74ea539ab61ed5b374e0c3f2164c62feb7dc530
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-02-16 15:43:49 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.11 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
@@ -1,16 +1,11 @@
+
 class Solution {
     fun solution(price: Int, money: Int, count: Int): Long {
         var answer: Long = 0
-        var sum: Long = 0
+        var total = 0L
         for(i in 1..count){
-            sum += (price * i).toLong()
-            
+            total += price * i
         }
-        answer = sum - money
-        if(answer <0)
-        return 0
-        
-        else
-        return answer
+        return if(money > total)0.toLong() else (total.toLong() - money.toLong())
     }
 }
