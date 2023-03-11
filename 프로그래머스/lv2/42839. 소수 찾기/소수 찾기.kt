7be7f1e1e6a2069d869dad025297e683f1cb1255
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-03-11 22:43:12 +0900

    [level 2] Title: 소수 찾기, Time: 15.06 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.kt
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.kt
@@ -1,31 +1,36 @@
 class Solution {
-         //에라토스테네스로 소수 구하기
-         //주어진 문자열로 만들 수 있는 수 구하기 -> 재귀로 구현
-    val setList = mutableSetOf<Int>()
-    
     fun solution(numbers: String): Int {
         var answer = 0
-        val prime = Array(20000000){true}
-        prime[0] = false
-        prime[1] = false
-        
-        for(i in 2..10000000){
-            if(prime[i]){
-                for(j in 2*i..10000000 step i)
-                prime[j] = false
+        val s = mutableSetOf<String>()
+        fun recursive(temp:String, others:String){
+            //비어있지 않으면 set에 추가
+            if(!temp.isEmpty()){
+                if(temp[0] != '0')
+                     s.add(temp)
             }
-        }        
-    makeNum("", numbers)
-    
-        return setList.filter{prime[it] == true}.count()
-    }
-    fun makeNum(number:String, others:String){
-        if(!number.equals(""))
-        setList.add(Integer.valueOf(number))
+            //한 글자씩 꺼내와 temp에 붙이기
+            for(i in 0 until others.length){
+                recursive(temp+others[i], others.substring(0, i)+others.substring(i+1))
+            }
         
-        for(i in 0..others.length-1){
-            makeNum(number + others[i], others.substring(0, i)+others.substring(i+1))
+       
+            
         }
+        recursive("", numbers)
+         s.forEach{
+            if(isprime(it.toInt())) answer++
+        }
+        
+        return answer
     }
     
+    fun isprime(num:Int):Boolean{
+        if(num == 0 || num == 1)return false
+        for(i in 2..num/2){
+            if(num%i == 0){
+                return false
+            } 
+        }
+        return true
+    }
 }
