fa349bbcbeb6431bd7f98a24276d5570d3caface
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-03-31 14:51:05 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.py
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -1,13 +1,11 @@
 def solution(clothes):
-    # 1. 옷을 종류별로 구분하기
     dic = {}
-    answer = 1
     for i, j in clothes:
-        dic[j] = dic.get(j, 0) + 1 #key값이 이미 있으면 기존값+1, 아니면 0 + 1
+        dic[j] = dic.get(j, 0) + 1 #옷 종류의 개수 넣어주기
         
-    # 2. 입지 않는 경우를 추가하여 모든 조합을 계산한다.
+    
+    answer = 1
     for key in dic:
-        answer *= (dic[key] + 1)
-    # 3. 아무 종류의 옷도 입지 않는 경우를 제외한다.
-
+        answer *= (dic[key]+1)
+        
     return answer - 1
