f41e8c488c4cc168ce9cbd1489e751ffec11bd54
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-05-23 13:20:37 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,20 +1,12 @@
 n = int(input())
-count = 0
-
+cnt = n
 for i in range(n):
-    m = str(input())
-    error = 0
-    for j in range(len(m) - 1):
-        if m[j] != m[j+1]:
-            new_word = m[j+1:]
-            if new_word.count(m[j]) > 0:
-                error += 1
-    if error == 0:
-      count += 1  
-
-print(count)
-
-
-
-
+    word = input()
+    for j in range(0, len(word) -1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt -= 1
+            break
+print(cnt)
 
