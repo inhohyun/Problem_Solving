ae7a15b81e4560eedd368cbe28a0c8fc416f3734
HoHyun In <96523102+inhohyun@users.noreply.github.com>
2023-05-24 09:34:13 +0900

    [Bronze I] Title: 이항 계수 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,4 +1,9 @@
-from math import factorial
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+    
 n, k = map(int, input().split())
-b = factorial(n) // (factorial(k) * factorial(n-k))
+b = factorial(n) //(factorial(k) * factorial(n-k))
 print(b)
