bc7c11c6fe0b1d2c6e55161fbd9b72d50997676a
HoHyun In <inhohyun@naver.com>
2023-06-28 22:54:02 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,31 +1,31 @@
-import sys
-
-input = sys.stdin.readline
-N = int(input())
+n = int(input())
 num = list(map(int, input().split()))
-op = list(map(int, input().split()))  # +, -, *, //
+op = list(map(int, input().split()))
 
-maximum = -1e9
-minimum = 1e9
+maxi = -1e9
+mini = 1e9
 
-
-def dfs(depth, total, plus, minus, multiply, divide):
-    global maximum, minimum
-    if depth == N:
-        maximum = max(total, maximum)
-        minimum = min(total, minimum)
+def dfs(depth, total, plus, minus, multi, divine):
+    global maxi, mini
+    if depth == n:
+        maxi = max(total, maxi)
+        mini = min(total, mini)
         return
+    # 연산을 다 했다면 최댓값, 최소값에 저장된 값과 비교하여 값을 갱신해줍니다.
 
     if plus:
-        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multi, divine)
     if minus:
-        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
-    if multiply:
-        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
-    if divide:
-        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
-
+        dfs(depth + 1, total - num[depth], plus, minus - 1, multi, divine)
+    if multi:
+        dfs(depth + 1, total * num[depth], plus, minus, multi - 1, divine)
+    if divine:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multi, divine - 1)
 
 dfs(1, num[0], op[0], op[1], op[2], op[3])
-print(maximum)
-print(minimum)
+# 1번째 연산이며 0번 인덱스의 저장된 값과 각 연산이 몇 번 남았는지를 입력하여 함수를
+# 실행하여줍니다.
+print(maxi)
+print(mini)
+
+# 이해가 안되는 부분 : 
