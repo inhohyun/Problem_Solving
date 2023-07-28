ea0fb578081f1e6f8f89ebfec61c32c52225fb0a
HoHyun In <inhohyun@naver.com>
2023-07-28 14:09:41 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 136 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,12 +1,6 @@
-a, b, c, d, e, f = map(int, input().split())
+a,b,c,d,e,f = map(int, input().split())
 
-# # 연립방정식은 런타임 에러가 뜨네
-# y = (c*d - f*a)//(b * d - a * e)
-# x = (c - b*y) // a
-# print(x, y)
-
-for i in range(-999, 1000):
-    for j in range(-999, 1000):
-        if a *i + b*j == c and d*i + e*j == f:
-            print(i, j)
-
+for x in range(-999, 1000):
+    for y in range(-999, 1000):
+        if a*x + b*y == c and d*x + e*y == f:
+            print(x, y)
