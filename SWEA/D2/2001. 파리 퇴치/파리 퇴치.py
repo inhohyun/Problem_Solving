2d79daae1d930e8c41aa260efa1bffa532d6db2f
HoHyun In <inhohyun@naver.com>
2023-08-04 01:25:55 +0900

    [D2] Title: 파리 퇴치, Time: 160 ms, Memory: 61,196 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,21 +1,19 @@
 test = int(input())
-
-for t in range(1, test+1):
+for T in range(1, test+1):
+    # N은 구역
+    # M은 파리채
+    N, M  = map(int, input().split())
     g = []
-    kills = []
-    n, m = map(int, input().split())
-    # 그래프에 값 추가
-    for _ in range(n):
-        numbers = list(map(int, input().split()))
-        g.append(numbers)
-    #n-m-1까지 반복문
-    for x in range(n-m+1):
-        for y in range(n-m+1):
-            temp = 0
-            for i in range(m):
-                for  j in range(m):
-                    temp += g[x+i][y+j]
-            kills.append(temp)
+    ans = 0
+    for _ in range(N):
+        g.append(list(map(int, input().split())))
     
-    ans = max(kills)
-    print(f'#{t}', ans)
+    for a in range(N-M+1):
+        for b in range(N-M+1):
+            cnt = 0
+            for i in range(a, a+M):
+                for j in range(b, b+M):
+                    cnt += g[i][j]
+            ans = max(ans, cnt)        
+    print(f'#{T}', ans)
+
