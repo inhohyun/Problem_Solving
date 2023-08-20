bc24f7134398cb781c119f86642c0b10abaa6975
HoHyun In <inhohyun@naver.com>
2023-08-20 10:43:48 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,16 +1,14 @@
 n, m = map(int, input().split())
 s = []
 
-def dfs():
+def dfs(start):
     if len(s) == m:
         print(' '.join(map(str, s)))
         return
     
-    for i in range(1, n+1):
-        if i not in s and len([value for value in s if value > i]) == 0:
+    for i in range(start, n+1):
             s.append(i)
-            dfs()
-            # return하고나면(한 줄 출력하고나면 끝자리를 빼고 다음 수 넣기 -> dfs로 수 하나씩 완성)
+            dfs(i+1)
             s.pop()
 
-dfs()
+dfs(1)
