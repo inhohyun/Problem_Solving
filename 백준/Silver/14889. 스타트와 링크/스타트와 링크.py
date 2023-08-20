697adc4ce3b4449a203d665a4977ae0e49ab389e
HoHyun In <inhohyun@naver.com>
2023-08-20 12:18:23 +0900

    [Silver II] Title: 스타트와 링크, Time: 5488 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,29 +1,32 @@
 
-def dfs(depth, idx):
-    global min_diff
+def dfs(depth, start):
+    global min_dif
     if depth == n//2:
-        power1, power2 = 0, 0
+        pow1, pow2 = 0,0
+
         for i in range(n):
             for j in range(n):
                 if visited[i] and visited[j]:
-                    power1 += graph[i][j]
+                    pow1 += s[i][j]
                 elif not visited[i] and not visited[j]:
-                    power2 += graph[i][j]
-        min_diff = min(min_diff, abs(power1-power2))
+                    pow2 += s[i][j]
+        min_dif = min(min_dif, abs(pow1- pow2))
         return
-
-    for i in range(idx, n):
+    
+    # 백트래킹으로 방문처리
+    for i in range(start, n):
         if not visited[i]:
             visited[i] = True
             dfs(depth+1, i+1)
             visited[i] = False
 
-
 n = int(input())
 
 visited = [False for _ in range(n)]
-graph = [list(map(int, input().split())) for _ in range(n)]
-min_diff = int(1e9)
+s = [list(map(int, input().split())) for _ in range(n)]
+min_dif = int(1e9)
 
-dfs(0, 0)
-print(min_diff)
+dfs(0,0)
+print(min_dif)
+
+
