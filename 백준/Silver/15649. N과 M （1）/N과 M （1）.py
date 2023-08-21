d91c8176c37d852497d981da577d8ddb8811d6fc
HoHyun In <inhohyun@naver.com>
2023-08-21 12:40:26 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,15 +1,16 @@
 n, m = map(int, input().split())
-s = []
+
+s =[]
 
 def dfs():
     if len(s) == m:
         print(' '.join(map(str, s)))
         return
     
-    for i in range(1, n+1):
-        if not i  in s:
+    for i in range(1,n+1):
+        if not i in s:
             s.append(i)
             dfs()
             s.pop()
-
+            
 dfs()
