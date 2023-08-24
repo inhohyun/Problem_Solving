486576f804b3e3d569c69e035e23181f2c66b47d
HoHyun In <inhohyun@naver.com>
2023-08-24 18:46:01 +0900

    [Silver II] Title: 연속합, Time: 92 ms, Memory: 38964 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,9 +1,5 @@
-# 하나씩 다 더하면 n^2아닌가? -> 다른 방식으로 풀어야함
-maxSum = -1001
-
 n = int(input())
 numbers = list(map(int, input().split()))
-cnt = 0
 
 for i in range(1, n):
     numbers[i] = max(numbers[i], numbers[i]+numbers[i-1])
