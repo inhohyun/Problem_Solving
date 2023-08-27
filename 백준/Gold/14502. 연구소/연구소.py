c9f65a41e41388dc23fedef50e2195174ad616ca
HoHyun In <inhohyun@naver.com>
2023-08-27 10:42:48 +0900

    [Gold IV] Title: 연구소, Time: 2976 ms, Memory: 129792 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -2,15 +2,16 @@ from collections import deque
 import copy
 
 def bfs():
-    queue = deque()
+    q = deque()
     tmp_graph = copy.deepcopy(graph)
+    # 2를 전부 퍼뜨리기 위해 2의 위치를 큐에 저장
     for i in range(n):
         for j in range(m):
             if tmp_graph[i][j] == 2:
-                queue.append((i, j))
-
-    while queue:
-        x, y = queue.popleft()
+                q.append((i,j))
+    
+    while q:
+        x,y = q.popleft()
 
         for i in range(4):
             nx = x + dx[i]
@@ -20,37 +21,45 @@ def bfs():
                 continue
             if tmp_graph[nx][ny] == 0:
                 tmp_graph[nx][ny] = 2
-                queue.append((nx, ny))
+                q.append((nx,ny))
 
+    # 안전구역 개수
     global answer
     cnt = 0
-
     for i in range(n):
-        cnt += tmp_graph[i].count(0)
-
+        for j in range(m):
+            if tmp_graph[i][j] == 0:
+                cnt += 1
+    
     answer = max(answer, cnt)
 
 
-def makeWall(cnt):
+
+def makWall(cnt):
     if cnt == 3:
         bfs()
         return
-
+    
     for i in range(n):
         for j in range(m):
             if graph[i][j] == 0:
                 graph[i][j] = 1
-                makeWall(cnt+1)
+                makWall(cnt+1)
                 graph[i][j] = 0
 
-n, m = map(int, input().split())
-graph = []
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
 
-for i in range(n):
+n, m = map(int, input().split())
+
+graph = []
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+for _ in range(n):
     graph.append(list(map(int, input().split())))
 
 answer = 0
-makeWall(0)
+makWall(0)
 print(answer)
+
+
