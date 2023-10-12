808fbaf049907abe8c6eb7897c93ecd54537be65
HoHyun In <inhohyun36@gmail.com>
2023-10-12 14:22:41 +0900

    [Bronze I] Title: 색종이, Time: 324 ms, Memory: 38304 KB, Score: 53 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,20 +1,25 @@
 n = int(input())
 arr = [[0] * 1001 for _ in range(1001)]
-for i in range(n):
+for k in range(1, n+1):
     x,y, width, height = map(int, input().split())
 
     for i in range(x, x+width):
         for j in range(y, y+height):
-            arr[j][i] += 1
+            arr[i][j] = k
 
 
 # 논리상 문제가 없어보이는데?
 # 각 색종이가 보이는 면적을 구하는것
-for nCnt in range(1, n+1):
-    cnt = 0
-    for i in range(1001):
-        for j in range(1001):
-            if arr[i][j] == nCnt:
-                cnt += 1
-    print(cnt)
+cnt_color = [0] * (n+1)
+for i in range(1001):
+    for j in range(1001):
+        if arr[i][j]:
+            cnt_color[arr[i][j]] += 1
+cnt_color.remove(0)
+for i in cnt_color:
+
+    print(i)
+
+
+
 
