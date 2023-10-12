53201a0b23b2a7f2d409d2939fd149ed0ae4c6d0
HoHyun In <inhohyun36@gmail.com>
2023-10-12 11:36:25 +0900

    [Bronze I] Title: 딱지놀이, Time: 216 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/14696. 딱지놀이/딱지놀이.py
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -1,34 +1,15 @@
-n = int(input())
+N = int(input())
 
-for i in range(n):
-    A = list(map(int, input().split()))
-    B = list(map(int, input().split()))
-    A.remove(A[0])
-    B.remove(B[0])
-    if A.count(4) > B.count(4):
-        print('A')
-    elif A.count(4) < B.count(4):
-        print('B')
+for _ in range(N):
+    temp_a = list(map(int, input().split()))[1:]  # 입력을 받되 맨 앞의 개수는 제외하고 저장한다.
+    temp_b = list(map(int, input().split()))[1:]  # 입력을 받되 맨 앞의 개수는 제외하고 저장한다.
 
-    if A.count(4) == B.count(4) and A.count(3) < B.count(3):
-        print('B')
-    elif A.count(4) == B.count(4) and A.count(3) > B.count(3):
-        print('A')
-
-    if A.count(3) == B.count(3) and A.count(2) < B.count(2):
-        print('B')
-    elif A.count(3) == B.count(3) and A.count(2) > B.count(2):
-        print('A')
-
-    if A.count(2) == B.count(2) and A.count(1) < B.count(1):
-        print('B')
-    elif A.count(2) == B.count(2) and A.count(1) > B.count(1):
-        print('A')
-
-    if sorted(A) == sorted(B):
-        print('D')
-
-
-
-
-
+    for i in range(4, 0, -1):  # 4부터 반대로 돈다.
+        if temp_a.count(i) > temp_b.count(i):  # 만약 해당 모양의 개수가 A가 더 많다면
+            print("A")
+            break
+        elif temp_a.count(i) < temp_b.count(i):  # 만약 해당 모양의 개수가 B가 더 많다면
+            print("B")
+            break
+        if i == 1:  # 만약 마지막까지 왔는데 break 되지 않았다면
+            print("D")  # D를 출력한다.
