84f10e953995ac5091d91d914dc38a3aadbf66e8
HoHyun In <inhohyun36@gmail.com>
2023-10-18 16:27:52 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 160 ms, Memory: 61,240 KB -BaekjoonHub

---
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -0,0 +1,33 @@
+t = int(input())
+for tc in range(1, t+1):
+    n, k = map(int, input().split())
+    data = []
+    for _ in range(n):
+        data.append(list(map(int, input().split())))
+
+    result = 0
+    # 가로 확인
+    for i in range(n):
+        cnt = 0
+        for j in range(n):
+            if data[i][j] == 1:
+                cnt += 1
+            if data[i][j] == 0 or j == n - 1:
+                if cnt == k:
+                    result += 1
+                if data[i][j] == 0:
+                    cnt = 0
+
+    # 세로 확인
+    for i in range(n):
+        cnt = 0
+        for j in range(n):
+            if data[j][i] == 1:
+                cnt += 1
+            if data[j][i] == 0 or j == n - 1:
+                if cnt == k:
+                    result += 1
+                if data[j][i] == 0:
+                    cnt = 0
+
+    print('#%d %d' % (tc, result))
