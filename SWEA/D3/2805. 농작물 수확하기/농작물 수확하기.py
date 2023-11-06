acac07e1c0423f9f9f46040442028ef87b1b3547
HoHyun In <inhohyun36@gmail.com>
2023-11-06 19:43:25 +0900

    [D3] Title: 농작물 수확하기, Time: 187 ms, Memory: 60,648 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,29 +1,22 @@
-# 범위가 위에서부터 2씩 증가하고 n부터 2씩 감소함
-
 t = int(input())
-
-for test in range(1, t+1):
+for tc in range(1,t+1):
     n = int(input())
     arr = []
-    for _ in range(n):
-        arr.append(list(map(int, input())))
-
-    answer = 0
-    left_tmp = n //2 
-    right_tmp = n // 2
-
     for i in range(n):
-        for j in range(left_tmp, right_tmp+1):
-            answer += arr[i][j]
+        arr.append(list(map(int, list(input()))))
+    ans = 0
+    mid = n // 2
+    l, r = mid,mid
+    for i in range(n):
+        ans += sum(arr[i][l:r+1])
 
-        if i < n//2:
-            left_tmp -= 1
-            right_tmp +=1
-        else:
-            left_tmp += 1
-            right_tmp -= 1
-        
-    
-    print('#{} {}'.format(test, answer))
+        if i < mid:
+            l -= 1
+            r += 1
+        elif i >= mid:
+            l += 1
+            r -= 1
+
+    print(f'#{tc}',ans)
 
 
