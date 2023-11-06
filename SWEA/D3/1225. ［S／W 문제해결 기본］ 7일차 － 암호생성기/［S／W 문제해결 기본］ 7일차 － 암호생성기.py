934cb5b622c006ebe120dbf30c3bd2c022856170
HoHyun In <inhohyun36@gmail.com>
2023-11-06 16:52:07 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 143 ms, Memory: 58,604 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,25 +1,26 @@
 from collections import deque
-for _ in range(10):
+for tc in range(1,11):
     t = int(input())
-    q = deque()
-    data = list(map(int, input().split()))
-    for i in data:
-        q.append(i)
-    flag = 0
-    while True:
-        if flag == 1:
-            break
-        # 5번 이동시키는 것이 한 사이클?
-        for i in range(1, 6):
-            
-            front = q.popleft()
-            front -= i
-            if front <= 0:
-                q.append(0)
-                flag = 1
-                break
-            else:
-                q.append(front)
-    
-    print(f'#{t}', ' '.join(map(str, q)))
+    number = list(map(int, input().split()))
+    q = deque(number)
 
+    i = 1
+    while True:
+        if q[-1] == 0:
+            break
+        if i == 6:
+            i = 1
+        tmp = q.popleft()
+        tmp -= i
+        if tmp < 0:
+            q.append(0)
+            break
+        q.append(tmp)
+
+
+        i += 1
+    ans = ''
+    while q:
+        ans += str(q.popleft()) + ' '
+
+    print(f'#{tc}',ans)
