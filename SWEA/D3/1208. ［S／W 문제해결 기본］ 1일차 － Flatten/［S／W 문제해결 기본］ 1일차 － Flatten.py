4b2afb528aa75ae92551bad2df4dabc5d0374949
HoHyun In <inhohyun36@gmail.com>
2023-11-06 15:48:59 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 215 ms, Memory: 59,976 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,21 +1,14 @@
-# 1208 Flatten
-
-for t in range(1, 11):
+for tc in range(1,11):
     n = int(input())
-    dum = list(map(int, input().split()))
-    # n번만큼 제일 큰수에서 1을 빼서 제일 작은수에 더하기
+    box = list(map(int, input().split()))
     for _ in range(n):
-        # 왜 1이나오지?
-        for index, value in enumerate(dum):
-            if value == max(dum):
-                dum[index] -= 1
-                for index2, value2 in enumerate(dum): 
-                    if value2 == min(dum):
-                        dum[index2] += 1
+        for i in range(len(box)):
+            if box[i] == max(box) and box[i] != 0:
+                box[i] -= 1
+                for j in range(len(box)):
+                    if box[j] == min(box) and box[j] != 0:
+                        box[j] += 1
                         break
+
                 break
-
-
-    
-
-    print(f'#{t}', max(dum)-min(dum))
+    print(f'#{tc}',max(box) - min(box))
