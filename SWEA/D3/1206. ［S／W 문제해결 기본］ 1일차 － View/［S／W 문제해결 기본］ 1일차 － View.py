6b1781504dd8ff51bc4a1dd8bffd880cc4b0e2db
HoHyun In <inhohyun36@gmail.com>
2023-11-06 14:29:05 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 118 ms, Memory: 59,720 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.py
@@ -1,19 +1,14 @@
-# 조망권이 확보된 층수
-for t in range(1, 11):
+for tc in range(1, 11):
     n = int(input())
     apart = list(map(int, input().split()))
-    # 양쪽으로 두 칸 이상이 확보되어야 함
-    ans = 0
-    for i in range(2, n-2):
-        # 양쪽보다 크다면
-        mx = apart[i-2]
-        for j in range(i-1, i+3):
-            if i==j:
-                continue
-            else:
-                if mx <apart[j]:
-                    mx = apart[j]
-        if apart[i] > mx:
-            ans += apart[i] - mx
-            
-    print(f'#{t}', ans)
+    cnt = 0
+    for i in range(2,n-2):
+        # 주변 두 칸 중 가장 높지 않으면 건너뜀
+        if apart[i] != max(apart[i-2:i+3]):
+            continue
+
+        second = sorted(apart[i-2:i+3])[-2]
+        # 두 번째로 높은 건물과 차이만큼 조명권 확보
+        cnt += apart[i] - second
+
+    print(f'#{tc}', cnt)
