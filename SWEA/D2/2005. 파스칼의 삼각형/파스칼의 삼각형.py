9743870d978a54edcc55843814330005bfe49cb2
HoHyun In <inhohyun36@gmail.com>
2023-11-10 14:56:16 +0900

    [D2] Title: 파스칼의 삼각형, Time: 100 ms, Memory: 45,568 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,31 +1,20 @@
-#2005 파스칼 삼각형
-test = int(input())
-# 이차원 배열로 표시
-for t in range(1, test+1):
+t = int(input())
+for tc in range(1, t+1):
     n = int(input())
-    paskal = [[0]*n for _ in range(n)]
+    arr = [[0] * n for _ in range(n)]
     for i in range(n):
-        paskal[i][0] = 1
-    if n >= 2:
-        paskal[1][1] = 1
-        for i in range(2, n):
-            for j in range(1, n):
-                paskal[i][j] = paskal[i-1][j-1] + paskal[i-1][j]
-        print(f'#{t}')
-        # 0은 뺴고 출력하기
-        for a in range(n):
-            for b in range(n):
-                if paskal[a][b] != 0:
-                    print(paskal[a][b], end=' ')
-            print()
-            
-         
+        arr[i][0] = 1
+    for i in range(1,n):
+        for j in range(1,n):
+            arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
 
-    
-    elif n == 1:
-        print(f'#{t}')
-        print(1)
-    elif n == 2:
-        print(f'#{t}')
-        print(1)
-        print(1, 1)
+    ans = ''
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] != 0:
+                ans += str(arr[i][j]) + ' '
+        if i == n-1:
+            break
+        ans += '\n'
+    print(f'#{tc}')
+    print(ans)
