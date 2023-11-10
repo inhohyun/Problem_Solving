1f144a597ceb8f5854f0ac97e2ecaff60a3ab440
HoHyun In <inhohyun36@gmail.com>
2023-11-10 14:27:37 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,497 ms, Memory: 246,156 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,31 +1,24 @@
-test = int(input())
-for t in range(1, test+1):
+t = int(input())
+for tc in range(1, t + 1):
     n = int(input())
     days = list(map(int, input().split()))
     buy = []
+    ans = 0
     max_cost = max(days)
-    answer = 0
     for i in range(n):
-        # 최대값이 아니면 구입
+
         if days[i] != max_cost:
             buy.append(days[i])
-
-        # 최대값이면 판매하고 최대값 갱신
         else:
-            answer += max_cost * len(buy) - sum(buy)
+            # 판매하기
+            ans += max_cost * len(buy) - sum(buy)
             buy.clear()
+
             if i == n-1:
                 break
-                
-            #최대값 갱신
+            # 최대값 갱신하기
             max_cost = 0
             for j in range(n-1, i, -1):
                 if max_cost < days[j]:
                     max_cost = days[j]
-
-
-    print(f'#{t}', answer)
-
-
-
-
+    print(f'#{tc}', ans)
