128912093a1547a1da48258072a3b5b7fd63fa8f
HoHyun In <inhohyun36@gmail.com>
2023-11-10 17:32:20 +0900

    [D2] Title: 간단한 소인수분해, Time: 96 ms, Memory: 45,060 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,21 +1,32 @@
-n = int(input())
+t = int(input())
+for tc in range(1, t+1):
+    n = int(input())
+    a,b,c,d,e = 0,0,0,0,0
 
-for i in range(1, n+1):
-    a = b = c = d = e = 0
-    num = int(input())
-    while(num % 2 == 0):
-        a += 1
-        num /= 2
-    while(num % 3 == 0):
-        b += 1
-        num /= 3
-    while(num % 5 == 0):
-        c += 1
-        num /= 5
-    while(num % 7 == 0):
-        d += 1
-        num /= 7
-    while(num % 11 == 0):
+    while True:
+        if n % 11 != 0:
+            break
+        n = n // 11
         e += 1
-        num /= 11
-    print('#'+str(i), a, b, c, d, e)
+    while True:
+        if n % 7 != 0:
+            break
+        n = n // 7
+        d += 1
+    while True:
+        if n % 5 != 0:
+            break
+        n = n // 5
+        c += 1
+    while True:
+        if n % 3 != 0:
+            break
+        n = n // 3
+        b += 1
+    while True:
+        if n % 2 != 0:
+            break
+        n = n // 2
+        a += 1
+    print(f'#{tc}',a,b,c,d,e)
+
