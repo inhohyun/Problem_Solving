6811e30864d19f2211c22143784a3e67b5ef8499
HoHyun In <inhohyun36@gmail.com>
2023-11-10 15:30:17 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 107 ms, Memory: 44,252 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,33 +1,31 @@
 t = int(input())
 for tc in range(1, t+1):
-    n, k = map(int, input().split())
-    data = []
-    for _ in range(n):
-        data.append(list(map(int, input().split())))
-
-    result = 0
-    # 가로 확인
+    n, m = map(int, input().split())
+    arr = [list(input().split()) for _ in range(n)]
+    ans = 0
     for i in range(n):
         cnt = 0
         for j in range(n):
-            if data[i][j] == 1:
+            if arr[i][j] == '1':
                 cnt += 1
-            if data[i][j] == 0 or j == n - 1:
-                if cnt == k:
-                    result += 1
-                if data[i][j] == 0:
-                    cnt = 0
-
-    # 세로 확인
+            elif arr[i][j] == '0':
+                if cnt == m:
+                    ans += 1
+                cnt = 0
+        if cnt == m:
+            ans += 1
+    cnt = 0
     for i in range(n):
         cnt = 0
         for j in range(n):
-            if data[j][i] == 1:
+            if arr[j][i] == '1':
                 cnt += 1
-            if data[j][i] == 0 or j == n - 1:
-                if cnt == k:
-                    result += 1
-                if data[j][i] == 0:
-                    cnt = 0
+            else:
+                if cnt == m:
+                    ans += 1
+                cnt = 0
+        if cnt == m:
+            ans += 1
+    print(f'#{tc}',ans)
 
-    print('#%d %d' % (tc, result))
+
