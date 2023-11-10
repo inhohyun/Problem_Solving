dc9aa1d4a9007e62ec954d80ff03893aa1d17179
HoHyun In <inhohyun36@gmail.com>
2023-11-10 18:14:54 +0900

    [D2] Title: 가랏! RC카!, Time: 98 ms, Memory: 45,608 KB -BaekjoonHub

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -2,23 +2,22 @@ t = int(input())
 for tc in range(1, t+1):
     n = int(input())
     ans = 0
-    speed = 0
+    # 현재 속도
+    current = 0
     for i in range(n):
-        command = input()
-        if command == '0':
-            ans += speed
-        else:
-            a, b = map(int, command.split())
-            # 가속
-            if a == 1:
-                speed += b
-            # 감속
-            elif a == 2:
-                speed -= b
-                if speed < 0:
-                    speed = 0
-            ans += speed
+        s = input()
+        # 0이 아닐떄
+        if len(s) != 1:
+            r, c = map(int, s.split())
+            if r == 1:
+                current += c
+            elif r == 2:
+                current -= c
+                # 속도가 0보다 줄진 않음
+                if current < 0:
+                    current = 0
+        #현재 속도 유지
+        ans += current
     print(f'#{tc}',ans)
-
 
 
