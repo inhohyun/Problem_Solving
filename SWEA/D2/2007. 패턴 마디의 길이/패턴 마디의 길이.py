c7b39e4237cf6b10b989b2ecb210f711cb41d921
HoHyun In <inhohyun36@gmail.com>
2023-11-10 16:22:52 +0900

    [D2] Title: 패턴 마디의 길이, Time: 95 ms, Memory: 45,064 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.py
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.py
@@ -1,8 +1,17 @@
-n = int(input())
-for test in range(1, n+1):
+t = int(input())
+for tc in range(1, t + 1):
     s = input()
-    #최대 10자리 이므로
-    for j in range(1, 10):
-        if s[:j] == s[j:2*j]:
-            print(f'#{test} {j}')
+    ans = 0
+    target = ''
+    for i in range(1, len(s)//2+1):
+        # 반복되는 단어 찾기
+        if s[:i] == s[i:i * 2]:
+            target = s[:i]
+            # 왜 target을 찾고 break를 걸면 target이 안나오는거지? -> 0일때도 성립하기 때문
+            print(f'#{tc}', i)
             break
+
+
+
+
+
