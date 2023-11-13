dc56a6b9a0d3bebccaee4ff47b6e9c5272d08e7d
HoHyun In <inhohyun36@gmail.com>
2023-11-13 10:55:49 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 112 ms, Memory: 48,256 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -1,11 +1,20 @@
-T = int(input())
-for tc in range(1, T+1):
-    init = list(input())
-    n = ['0']*len(init)
-    cnt = 0
-    for i in range(len(n)):
-        if n[i] != init[i]:
-            n[i:] = init[i]*len(n[i:])
-            cnt += 1
+t = int(input())
+for tc in range(1, t+1):
+    s = input()
+    ans = 0
+    while True:
+        if s.count('0') == len(s):
 
-    print('#{} {}'.format(tc, cnt))
+            break
+
+        for i in range(len(s)):
+            # 뒤에 전부 반전시키기
+            if s[i] == '1':
+                for j in range(i, len(s)):
+                    if s[j] == '1':
+                        s = s[:j] + '0'+s[j+1:]
+                    else:
+                        s = s[:j] + '1'+s[j+1:]
+
+                ans += 1
+    print(f'#{tc}',ans)
