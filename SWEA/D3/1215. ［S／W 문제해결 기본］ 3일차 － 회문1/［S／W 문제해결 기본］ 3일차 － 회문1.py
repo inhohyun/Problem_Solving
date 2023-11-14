8377d1c7998fd72358fe41cc9cf249652e66fc6e
HoHyun In <inhohyun36@gmail.com>
2023-11-14 17:10:57 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 106 ms, Memory: 51,720 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.py
@@ -1,31 +1,19 @@
-for t in range(1, 11):
-    n = int(input())
-    ans = 0
-    # 8*8 배열 입력받기
-    arr = []
-    for i in range(8):
-        arr.append(list(input()))
 
-    str_target = ''
+for tc in range(1, 11):
+    n = int(input())
+    arr = [list(input()) for _ in range(8)]
+    ans = 0
     # 가로 탐색
     for i in range(8):
-        for j in range(9-n):
-            for b in range(j, j+n):
-                str_target += arr[i][b]
-            
-            if str_target == str_target[::-1]:
+        for j in range(8-n+1):
+            if arr[i][j:j+n] == arr[i][j:j+n][::-1]:
                 ans += 1
-            str_target = ''
-
     # 세로 탐색
-    for i in range(9-n):  
-        for j in range(8):
-            # index out 에러 해결하기
-            for a in range(i, i+n):
-                str_target += arr[a][j]
-            
-            if str_target == str_target[::-1]:
+    for i in range(8):
+        for j in range(8-n+1):
+            tmp = ''
+            for a in range(j, j+n):
+                tmp += arr[a][i]
+            if tmp == tmp[::-1]:
                 ans += 1
-            str_target = ''
-
-    print(f'#{t}',ans)
+    print(f'#{tc}',ans)
