8c7167978cd8559706391d18405162c774de9ec3
HoHyun In <inhohyun36@gmail.com>
2023-11-14 18:59:16 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 93 ms, Memory: 42,724 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.py
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.py
@@ -1,12 +1,14 @@
 for tc in range(1, 11):
-    n, s = input().split()
+    n, number = input().split()
     n = int(n)
-    s = str(s)
-    stack = []
-    for o in s:
-        if stack and o == stack[-1]:
-            stack.pop()
-        else:
-            stack.append(o)
+    stack = [number[0]]
+    for i in range(1, n):
+        if len(stack) == 0:
+            stack.append(number[i])
 
+        elif stack[-1] == number[i]:
+            stack.pop()
+
+        else:
+            stack.append(number[i])
     print(f'#{tc}',''.join(stack))
