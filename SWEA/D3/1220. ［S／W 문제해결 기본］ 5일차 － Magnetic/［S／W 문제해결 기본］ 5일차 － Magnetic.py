2036b019f15f7e8b2a695f93cd5a12a950306f68
HoHyun In <inhohyun36@gmail.com>
2023-11-15 10:24:50 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 180 ms, Memory: 60,156 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.py
@@ -1,24 +1,14 @@
 for tc in range(1, 11):
     n = int(input())
-    
-    arr = []
-    cnt = 0
-    # 100 * 100 행렬 입력받기
-    for _ in range(100):
-        arr.append(list(map(int, input().split())))
+    arr = [list(map(int, input().split())) for _ in range(100)]
 
-    # 세로(열)을 기준으로 리스트화하기
-    row_arr = [[0] *100 for _ in range(100)]
+    ans = 0
+    tmp = ''
+    # 세로 기준으로 12의 개수를 셈
     for i in range(100):
         for j in range(100):
-            row_arr[i][j] = arr[j][i]
-    
-    # 바꾼 리스트에서 0을 모두 제거하기
-    for i in range(100):
-        str_row_arr = ''.join(map(str, row_arr[i]))
-        str_row_arr = str_row_arr.replace('0', '')
-        # 12의 개수를 세고 정답 출력하기
-        cnt += str_row_arr.count('12')
-
-    print(f'#{tc}',cnt)
-    
+            if arr[j][i] != 0:
+                tmp += str(arr[j][i])
+        ans += tmp.count('12')
+        tmp = ''
+    print(f'#{tc}',ans)
