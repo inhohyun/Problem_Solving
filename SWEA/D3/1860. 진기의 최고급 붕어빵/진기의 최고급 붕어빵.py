5835003c5cb0686fc8353671ff5333a442e97547
HoHyun In <inhohyun36@gmail.com>
2023-11-17 16:16:41 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 483 ms, Memory: 62,032 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -1,26 +1,32 @@
 t = int(input())
 for tc in range(1, t+1):
-    # m 초마다 k개의 붕어빵을 만듦
     n, m, k = map(int, input().split())
     person = list(map(int, input().split()))
-    rest_boog = 0 # 만들어논 붕어빵 개수
-    flag = 0
-    # 손님 정렬
+    # 온 사람들 정렬시키기
     person.sort()
-    for i in range(n):
-        time = person[i]
-        # time 지점에 남은 붕어빵이 몇개인지
-        # m초에 k개 만들어지는데 그럼 time시간에 몇개 만들어져있는지?
-        rest_boog = ((time //m) * k) - (i)
-        if rest_boog < 1:
-            print(f'#{tc}','Impossible')
+    rest = 0
+    i = 0
+    flag = 0
+    # 1초부터 마지막 사람이 온 시간까지
+    for time in range(1, person[-1]+1):
+        if person[0] == 0:
             flag = 1
             break
+        # 붕어빵 나올시간 되면 붕어빵 만들기
+        if time % m == 0:
+            rest += k
+        # 사람이 온 경우
+        if time == person[i]:
+            if rest != 0:
+                rest -= 1
+                # 다음 사람
+                i += 1
+            # 남은 붕어빵이 없는데 사람이 올 경우
+            else:
+                flag = 1
+                break
 
-    if flag == 0:
-         print(f'#{tc}', 'Possible')
-
-
-
-
-
+    if flag == 1:
+        print(f'#{tc}', 'Impossible')
+    else:
+        print(f'#{tc}', 'Possible')
