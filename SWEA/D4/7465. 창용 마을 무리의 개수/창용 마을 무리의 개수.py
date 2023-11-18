8842421978df4e0446297b4489a70b05463f4724
HoHyun In <inhohyun36@gmail.com>
2023-11-18 18:07:51 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 218 ms, Memory: 61,364 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
@@ -4,26 +4,26 @@ def bfs(v):
     q = deque()
     q.append(v)
     while q:
-        v = q.pop()
-        for i in range(1,n+1):
-            if arr[i][v] == 1 and visited[i] == False:
-                visited[i] = True
+        v = q.popleft()
+        for i in range(1, n+1):
+            if visited[i] == 0 and graph[v][i] == 1:
+                visited[i] = 1
                 q.append(i)
 
 t = int(input())
 for tc in range(1, t+1):
-    n,m = map(int, input().split())
-    arr = [[0] * (n+1) for _ in range(n+1)]
-    visited = [False for _ in range(n+1)]
-    ans = 0
+    n, m = map(int, input().split())
+    graph = [[0] *(n+1) for _ in range(n+1)]
     for _ in range(m):
-        a,b = map(int, input().split())
-        arr[a][b] = 1
-        arr[b][a] = 1
-    for i in range(1,n+1):
-        if visited[i] == False:
+        x, y = map(int, input().split())
+        graph[x][y] = 1
+        graph[y][x] = 1
+
+    visited = [0] * (n+1)
+    cnt = 0
+    for i in range(1, n+1):
+        if visited[i] == 0:
             bfs(i)
-            ans += 1
+            cnt += 1
 
-
-    print(f'#{tc}',ans)
+    print(f'#{tc}',cnt)
