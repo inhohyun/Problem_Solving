d757e41d9efbe97d9e77dda85d76de9efad222a3
HoHyun In <inhohyun36@gmail.com>
2024-01-31 01:13:41 +0900

    [Silver I] Title: 숨바꼭질, Time: 156 ms, Memory: 18420 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -12,10 +12,35 @@ public class Main {
 		int k = Integer.parseInt(st.nextToken());
 
 		System.out.println(bfs(n, k));
+//		dfs(n,k,0);
+//		System.out.println(ans);
 
 	}
-
-	static int[] visited = new int[200001];
+//	static int ans;
+//	static void dfs(int start, int end, int tmp) {
+//		//값을 찾았을 때
+//		if(start == end) {
+//			ans = tmp;
+//			Thread.currentThread().interrupt();
+//			return;
+//		}
+//		// 값이 조건에서 벗어나 더 연산해도 못 찾을 때
+//		if(start <=0 || start > end) {
+//			return;
+//		}
+//		//3가지 경우를 모두 탐색
+//		dfs(start+1, end, tmp+1);
+//		//값을 못찾아 이전 값으로 리턴했으니 연산횟수 -1 
+//		tmp -= 1;
+//		dfs(start-1, end, tmp+1);
+//		tmp -= 1;
+//		dfs(start*2, end, tmp+1);
+//		return;
+//	}
+	
+	
+	
+	static int[] visited = new int[100001];
 
 	static int bfs(int start, int end) {
 		Queue<Integer> q = new LinkedList<>();
@@ -28,16 +53,16 @@ public class Main {
 			}
 			// 3가지 조건 모두 적용해보기
 			// 1간 전진한 값이 범위 안이고 아직 안 만들어본 값일 때
-			if (current + 1 >= 0 && current + 1 <= 200000 && visited[current + 1] == 0) {
-				q.add(current+1);
+			if (current + 1 >= 0 && current + 1 <= 100000 && visited[current + 1] == 0) {
+				q.add(current + 1);
 				visited[current + 1] = visited[current] + 1;
 			}
-			if (current - 1 >= 0 && current - 1 <= 200000 && visited[current - 1] == 0) {
-				q.add(current-1);
+			if (current - 1 >= 0 && current - 1 <= 100000 && visited[current - 1] == 0) {
+				q.add(current - 1);
 				visited[current - 1] = visited[current] + 1;
 			}
-			if (current * 2 >= 0 && current * 2 <= 200000 && visited[current * 2] == 0) {
-				q.add(current*2);
+			if (current * 2 >= 0 && current * 2 <= 100000 && visited[current * 2] == 0) {
+				q.add(current * 2);
 				visited[current * 2] = visited[current] + 1;
 			}
 		}
