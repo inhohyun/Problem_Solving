17199f11fa590a43429a7440806adbc0d5d992b1
HoHyun In <inhohyun36@gmail.com>
2025-03-18 18:28:40 +0900

    [level 2] Title: 구명보트, Time: 12.80 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,9 +1,9 @@
 function solution(people, limit) {
     let cnt = 0;
-    let sorted_people = people.sort();
-    let index = 0;
-    for(let i = sorted_people.length- 1; i >= index ; i--){
-       if(sorted_people[i] + sorted_people[index] <= limit){
+    let index = 0;  
+    people.sort((a,b) => a - b);
+    for(let i = people.length- 1; i >= index ; i--){
+       if(people[i] + people[index] <= limit){
            index++;
        }
         
