37e35c700a13707dc820ce819155fdcd953d0d18
HoHyun In <inhohyun36@gmail.com>
2025-06-28 17:56:12 +0900

    [level 2] Title: 프로세스, Time: 0.76 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,21 +1,31 @@
 function solution(priorities, location) {
-    let arr = priorities.map((value, index) => [value, index === location]);
-    let cnt = 0;
-
-    while (arr.length > 0) {
-        const current = arr.shift();
-        const hasHigher = arr.some(item => item[0] > current[0]);
-
-        if (hasHigher) {
-            // 더 높은 우선순위가 있으면 다시 뒤로 보냄
+    var answer = 0;
+    let arr = priorities.map((value, index)=>{
+        if(index === location){
+            return [value, true];
+        }else{
+            return [value, false];
+        }
+    });
+    
+    while(arr.length){
+        let current = arr.shift();
+        let hasHigher = arr.some(item => item[0] > current[0]);
+        
+        // 우선순위가 더 높은게 있을 경우
+        if(hasHigher){
+            // 얌전히 다시 넣기
             arr.push(current);
-        } else {
-            // 현재 값 출력 처리
-            cnt++;
-            if (current[1]) {
-                // 내가 찾던 작업이면
-                return cnt;
+        }else{
+            // 빼도 되는 값일 경우
+            answer++;
+            // 찾는 값일 경우
+            if(current[1]){
+                return answer;
             }
         }
+        
+        
     }
+    return answer;
 }
