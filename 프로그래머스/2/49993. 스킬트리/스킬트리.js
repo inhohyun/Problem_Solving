324ec64ea99c94854b1c3afd21a12cd60e8beb10
HoHyun In <inhohyun36@gmail.com>
2025-07-23 11:18:13 +0900

    [level 2] Title: 스킬트리, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.js
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.js
@@ -1,15 +1,17 @@
 function solution(skill, skill_trees) {
-    var answer = 0;
     
-    skill_trees.forEach((value) =>{
-        //skill에 없는 스킬은 제거하기
-        let temp = value.split('').filter(char => skill.includes(char)).join('');
-        // console.log(temp);
+    
+    const req = {};
+    skill.split('').forEach((char)=>  req[char] = true);
+    
+    const answer = skill_trees.filter((char)=> {
         
-        //순서
-        if(skill.indexOf(temp) === 0){
-            answer++;
-        }
-    })
+        // 순서 스킬만 필터링된 스킬트리
+        const filtered_char = char.split('').filter((c) =>{
+            return req[c];
+        }).join('');
+        
+        return skill.startsWith(filtered_char);
+    }).length;
     return answer;
 }
